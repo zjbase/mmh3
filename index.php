1bc2bc83090b735e6541f4358f9e4be15d590ca1
zjpro <phpsir@qq.com>
2021-01-13 17:29:46 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,18 +3,13 @@ require __DIR__ . '/vendor/autoload.php';
 use lastguest\Murmur;
 ?>
 <html>
- 
-
-
-
- 
 <body>
 
 	<?php
 	 if($_POST)
 	 {
 
-	 	$cc=file_get_contenst($_POST["icourl"]);
+	 	$cc=file_get_contents($_POST["icourl"]);
 	 	$cc=base64_encode($cc);
 		echo Murmur::hash3($cc);
 	 	 
@@ -23,7 +18,7 @@ use lastguest\Murmur;
  
 
 	<form action="index.php" method="post">
-		icourl: <input type="text" name="icourl"><br>
+		icourl: <input type="text" name="icourl" value="http://115.152.145.213:81/favicon.ico"><br>
 		 
 		<input type="submit">
 	</form>
