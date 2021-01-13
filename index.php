50d74da0288774c5d9c6bf9308439b087c09ba49
zjpro <phpsir@qq.com>
2021-01-13 17:19:07 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+ 
+
+
+
+ 
+<body>
+
+	<?php
+	 if($_POST)
+	 {
+	 	var_dump($_POST);
+	 }
+?>
+ 
+
+	<form action="index.php" method="post">
+		icourl: <input type="text" name="icourl"><br>
+		 
+		<input type="submit">
+	</form>
+</body>
+</html>
